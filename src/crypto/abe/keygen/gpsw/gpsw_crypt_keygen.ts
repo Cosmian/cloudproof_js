552d61ceb036adba7ef020c964e030cc2f07555b
Bruno Grieder <bruno.grieder@cosmian.com>
2022-06-26 12:42:03 +0200

    re-importing from old repo

--- src/crypto/abe/keygen/gpsw/gpsw_crypt_keygen.ts
+++ src/crypto/abe/keygen/gpsw/gpsw_crypt_keygen.ts
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 5706751 (re-importing from old repo)
 // /* tslint:disable:max-classes-per-file */
 // import {
 //   webassembly_generate_master_keys,
@@ -9,6 +12,7 @@
 // import { fromBeBytes, hexDecode } from "../../../../utils/utils"
 // import { AbeKeyGeneration, AbeMasterKey } from "../keygen"
 // import { Policy } from "../../policy"
+<<<<<<< HEAD
 =======
 /* tslint:disable:max-classes-per-file */
 import {
@@ -58,3 +62,38 @@ import { Policy } from "../../policy"
 =======
 }
 >>>>>>> 5036a76 (progressing on policy ipl)
+=======
+
+// export class GpswMasterKey extends AbeMasterKey {
+
+// }
+
+// export class GpswMasterKeyGeneration extends AbeKeyGeneration {
+//   public generateMasterKey(policy: Policy): AbeMasterKey {
+//     logger.log(() => "policy: " + policy)
+
+//     const policyBytes = policy.toJsonEncoded()
+//     const masterKeys = webassembly_generate_master_keys(policyBytes)
+//     const privateKeySize = fromBeBytes(masterKeys.slice(0, 4))
+//     logger.log(() => "private key size: " + privateKeySize)
+//     return new AbeMasterKey(
+//       masterKeys.slice(4, 4 + privateKeySize),
+//       masterKeys.slice(4 + privateKeySize, masterKeys.length)
+//     )
+//   }
+
+//   public generateUserPrivateKey(privateKey: Uint8Array, accessPolicy: string, policy: Policy): Uint8Array {
+//     logger.log(() => "privateKey: " + privateKey)
+//     logger.log(() => "accessPolicy: " + accessPolicy)
+//     logger.log(() => "policy: " + policy)
+
+//     const policyBytes = policy.toJsonEncoded()
+//     const userPrivateKey = webassembly_generate_user_private_key(privateKey, accessPolicy, policyBytes)
+
+//     return userPrivateKey
+//   }
+
+
+
+// }
+>>>>>>> 5706751 (re-importing from old repo)
--- src/site/index.ts
+++ src/site/index.ts
@@ -18,10 +18,14 @@ import { GpswHybridEncryption } from "../crypto/abe/hybrid_crypto/gpsw/encryptio
 import { EncryptedEntry, WorkerPool } from "../crypto/abe/hybrid_crypto/worker_pool"
 import { CoverCryptMasterKeyGeneration } from "../crypto/abe/keygen/cover_crypt/cover_crypt_keygen"
 <<<<<<< HEAD
+<<<<<<< HEAD
 // import { GpswMasterKeyGeneration } from "../crypto/abe/keygen/gpsw/gpsw_crypt_keygen"
 =======
 import { GpswMasterKeyGeneration } from "../crypto/abe/keygen/gpsw/gpsw_crypt_keygen"
 >>>>>>> 54f95b8 (re-importing from old repo)
+=======
+// import { GpswMasterKeyGeneration } from "../crypto/abe/keygen/gpsw/gpsw_crypt_keygen"
+>>>>>>> 5706751 (re-importing from old repo)
 import * as lib from "../lib"
 import { logger } from "./../utils/logger"
 import { hexDecode } from "./../utils/utils"
@@ -47,23 +51,24 @@ import { EncryptedFileReader } from "../files/upload/EncryptedFileReader"
 import { DecryptionTransformStream } from "../files/transformers/DecryptionTransformStream"
 
 
-class DB implements DBInterface {
-  instance: AxiosInstance = axios.create({
-    baseURL: process.env.SERVER,
-    timeout: 15000,
-  });
+// class DB implements DBInterface {
+//   instance: AxiosInstance = axios.create({
+//     baseURL: process.env.SERVER,
+//     timeout: 15000,
+//   });
 
-  responseBody = (response: AxiosResponse) => response.data;
+//   responseBody = (response: AxiosResponse) => response.data;
 
-  requests = {
-    get: (url: string) => this.instance.get(url).then(this.responseBody),
-  };
+//   requests = {
+//     get: (url: string) => this.instance.get(url).then(this.responseBody),
+//   };
 
-  getEntryTableEntries(uids: string[]): Promise<{ uid: string; Value: string }[]> {
-    return this.requests.get(`/index_chain?UID=in.(${uids})`)
-  }
+//   getEntryTableEntries(uids: string[]): Promise<{ uid: string; Value: string }[]> {
+//     return this.requests.get(`/index_chain?UID=in.(${uids})`)
+//   }
 
 
+<<<<<<< HEAD
   getChainTableEntries(uids: string[]): Promise<{ uid: string; Value: string }[]> {
     return this.requests.get(`/index_entry?UID=in.(${uids})`)
   }
@@ -95,6 +100,16 @@ class DB implements DBInterface {
 //     return this.requests.get(`/encrypted_directory?UID=in.(${uids})`)
 //   }
 
+=======
+//   getChainTableEntries(uids: string[]): Promise<{ uid: string; Value: string }[]> {
+//     return this.requests.get(`/index_entry?UID=in.(${uids})`)
+//   }
+
+//   getEncryptedDirectoryEntries(uids: string[]): Promise<{ uid: string, Enc_K_base: string, Enc_K_rh: string, Enc_K_sec: string }[]> {
+//     return this.requests.get(`/encrypted_directory?UID=in.(${uids})`)
+//   }
+
+>>>>>>> 5706751 (re-importing from old repo)
 //   getFirstEncryptedDirectoryEntries(): Promise<{ uid: string, Enc_K_base: string, Enc_K_rh: string, Enc_K_sec: string }[]> {
 //     const config = {
 //       headers: {
@@ -104,6 +119,7 @@ class DB implements DBInterface {
 //     }
 //     return this.instance.get(`/encrypted_directory`, config).then(this.responseBody)
 //   }
+<<<<<<< HEAD
 
 //   getFirstUsers(): Promise<object[]> {
 //     const config = {
@@ -179,37 +195,40 @@ class DB implements DBInterface {
     }
     return this.instance.get(`/encrypted_directory`, config).then(this.responseBody)
   }
+=======
+>>>>>>> 5706751 (re-importing from old repo)
 
-  getFirstUsers(): Promise<object[]> {
-    const config = {
-      headers: {
-        "Range-Unit": "items",
-        "Range": "0-4",
-      }
-    }
-    return this.instance.get(`/users`, config).then(this.responseBody)
-  }
-}
+//   getFirstUsers(): Promise<object[]> {
+//     const config = {
+//       headers: {
+//         "Range-Unit": "items",
+//         "Range": "0-4",
+//       }
+//     }
+//     return this.instance.get(`/users`, config).then(this.responseBody)
+//   }
+// }
 
-async function loadData() {
-  const db = new DB()
-  const users = await db.getFirstUsers()
-  const encryptedUsers = await db.getFirstEncryptedDirectoryEntries()
-  const clearDb = document.getElementById("clear_db")
-  const encDb = document.getElementById("enc_db")
-  if (clearDb && encDb) {
-    if (clearDb.innerHTML || encDb.innerHTML) {
-      clearDb.innerHTML = ""
-      encDb.innerHTML = ""
-    }
-    else {
-      displayInTab(users, clearDb)
-      displayInTab(encryptedUsers, encDb)
-    }
-  }
-};
-(window as any).loadData = loadData
+// async function loadData() {
+//   const db = new DB()
+//   const users = await db.getFirstUsers()
+//   const encryptedUsers = await db.getFirstEncryptedDirectoryEntries()
+//   const clearDb = document.getElementById("clear_db")
+//   const encDb = document.getElementById("enc_db")
+//   if (clearDb && encDb) {
+//     if (clearDb.innerHTML || encDb.innerHTML) {
+//       clearDb.innerHTML = ""
+//       encDb.innerHTML = ""
+//     }
+//     else {
+//       displayInTab(users, clearDb)
+//       displayInTab(encryptedUsers, encDb)
+//     }
+//   }
+// };
+// (window as any).loadData = loadData
 
+<<<<<<< HEAD
 /**
  * Display an array of simple JS objects into a an array in HTML
  * @param array array to display
@@ -245,6 +264,42 @@ function displayInTab(array: object[], parent: HTMLElement) {
   })
 }
 >>>>>>> 54f95b8 (re-importing from old repo)
+=======
+// /**
+//  * Display an array of simple JS objects into a an array in HTML
+//  * @param array array to display
+//  * @param parent HTML parent element to insert the line in
+//  * @returns void
+//  */
+// function displayInTab(array: object[], parent: HTMLElement) {
+//   array.forEach((item, index) => {
+//     if (item) {
+//       if (index === 0) {
+//         const columns = document.createElement('div')
+//         columns.setAttribute('class', "item columns")
+//         const keys = Object.keys(item)
+//         for (const key of keys) {
+//           const column = document.createElement('div')
+//           column.setAttribute("class", "cell")
+//           column.innerHTML = key
+//           columns.appendChild(column)
+//         }
+//         parent.appendChild(columns)
+//       }
+//       const line = document.createElement('div')
+//       line.setAttribute("class", "item")
+//       const values = Object.values(item)
+//       for (const value of values) {
+//         const cell = document.createElement('div')
+//         cell.setAttribute("class", "cell")
+//         cell.innerHTML = value
+//         line.appendChild(cell)
+//       }
+//       parent.appendChild(line)
+//     }
+//   })
+// }
+>>>>>>> 5706751 (re-importing from old repo)
 
 /**
  * Display No result in div
@@ -267,6 +322,7 @@ function sanitizeString(str: string): string {
   return str.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^\w\-]+/g, '-')
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 // //
 // /**
@@ -364,6 +420,8 @@ function sanitizeString(str: string): string {
 
 
 =======
+=======
+>>>>>>> 5706751 (re-importing from old repo)
 //
 /**
  * Search terms with Findex implementation
@@ -444,7 +502,21 @@ async function search(words: string, role: string, logicalSwitch: boolean) {
 async function search(words: string, role: string) {
   type EncryptedValue = { uid: string, Enc_K_base: string, Enc_K_rh: string, Enc_K_sec: string }
   type ClearValue = { User: string, HR_Elements: string, Security_Elements: string }
+=======
+// //
+// /**
+//  * Search terms with Findex implementation
+//  * @param words string of all searched terms separated by a space character
+//  * @param role chosen role to decrypt result
+//  * @param logicalSwitch boolean to select OR (false) AND (true) operator
+//  * @returns void
+//  */
+// async function search(words: string, role: string) {
+//   type EncryptedValue = { uid: string, Enc_K_base: string, Enc_K_rh: string, Enc_K_sec: string }
+//   type ClearValue = { User: string, HR_Elements: string, Security_Elements: string }
+>>>>>>> 0ad6224 (re-importing from old repo)
 
+<<<<<<< HEAD
   const result = document.getElementById("result")
   const content = document.getElementById("content")
   if (result && content) {
@@ -519,12 +591,85 @@ async function search(words: string, role: string) {
 (window as any).search = search
 =======
 >>>>>>> 5036a76 (progressing on policy ipl)
+=======
+//   const result = document.getElementById("result")
+//   const content = document.getElementById("content")
+//   if (result && content) {
+//     result.style.visibility = "visible"
+//     content.innerHTML = ""
+//     try {
+//       const db = new DB()
+//       const k1 = "19e1b63d2972a47b84194ed5fa6d8264fc8cbe6dfee5074c8fb1eac3a17b85e8"
+//       const k2 = "a2cdd03bf58eea8ae842e06ae351700cbac94c8a5dbd8f38984dfa5c104f59d0"
+//       const queryResults = await Findex.query(k1, k2, words.split(" ").map(word => sanitizeString(word)), db, 100)
+//       if (queryResults) {
+//         const res: EncryptedValue[] = await db.getEncryptedDirectoryEntries(queryResults.reduce((acc, queryResult) => { return [...acc, ...queryResult.dbUids] }, [] as string[]))
+//         if (res && res.length) {
+//           switch (role) {
+//             case "mallory":
+//               displayInTab(res, content)
+//               break
+//             case "alice":
+//             case "bob":
+//               let key
+//               if (role === "bob") {
+//                 key = bobKey
+//               }
+//               else {
+//                 key = aliceKey
+//               }
+//               const hybridDecryption = new GpswHybridDecryption(hexDecode(key))
+//               const clearValues: ClearValue[] = []
+//               res.forEach((item) => {
+//                 if (item) {
+//                   const clearValue: ClearValue = { User: "", HR_Elements: "", Security_Elements: "" }
+//                   const clearKeys = Object.keys(clearValue) as (keyof ClearValue)[]
+//                   const encryptedKeys = Object.keys(item) as (keyof EncryptedValue)[]
+//                   for (let index = 0; index < clearKeys.length; index++) {
+//                     try {
+//                       const itemKey = encryptedKeys[index * 2 + 1]
+//                       const clearKey = clearKeys[index]
+//                       const clearText = hybridDecryption.decrypt(hexDecode(item[itemKey].substring(2)))
+//                       const value = new TextDecoder().decode(clearText).match(/'([^']+)'/g)
+//                       if (value) {
+//                         clearValue[clearKey] = value.map(val => { return val.slice(1, -1) }).join(" | ")
+//                       }
+//                     }
+//                     catch (e) {
+//                       logger.log(() => "Impossible to decrypt")
+//                     }
+//                   }
+//                   if (clearValue.User || clearValue.HR_Elements || clearValue.Security_Elements) {
+//                     clearValues.push(clearValue)
+//                   }
+//                 }
+//               })
+//               if (clearValues.length) {
+//                 displayInTab(clearValues, content)
+//               } else {
+//                 displayNoResult(content)
+//               }
+//               hybridDecryption.destroyInstance()
+//           }
+//         } else {
+//           displayNoResult(content)
+//         }
+//       } else {
+//         displayNoResult(content)
+//       }
+//     } catch {
+//       displayNoResult(content)
+//     }
+//   }
+// }
+>>>>>>> 5706751 (re-importing from old repo)
 
 
 // ----------------------------------------------------
 // Local files encryption and decryption
 // ----------------------------------------------------
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 
@@ -596,6 +741,10 @@ async function decrypt_file(file: File): Promise<void> {
 >>>>>>> 54f95b8 (re-importing from old repo)
 =======
 (window as any).search = search;
+=======
+// (window as any).search = search;
+(window as any).encrypt_files = encrypt_files;
+>>>>>>> 5706751 (re-importing from old repo)
 (window as any).encrypt_file = encrypt_file;
 (window as any).decrypt_files = decrypt_files;
 (window as any).decrypt_file = decrypt_file
@@ -829,12 +978,16 @@ const displayError = (err: string) => {
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 5706751 (re-importing from old repo)
 // // run demo scenario for ABE implementation
 // function abeDemo(): string {
 //   if (!isGpswImplementation()) {
 //     // const policy =     new CoverCryptPolicy(100)
 //     //   .addAxis("Security Level", ["Protected", "Low Secret", "Medium Secret", "High Secret", "Top Secret"], true)
 //     //   .addAxis("Department", ["R&D", "HR", "MKG", "FIN"], false)
+<<<<<<< HEAD
 
 //     const keyGeneration = new CoverCryptMasterKeyGeneration()
 //     const demoKeys = new CoverCryptDemoKeys()
@@ -870,18 +1023,21 @@ function abeDemo(): string {
     // const policy =     new CoverCryptPolicy(100)
     //   .addAxis("Security Level", ["Protected", "Low Secret", "Medium Secret", "High Secret", "Top Secret"], true)
     //   .addAxis("Department", ["R&D", "HR", "MKG", "FIN"], false)
+=======
+>>>>>>> 5706751 (re-importing from old repo)
 
-    const keyGeneration = new CoverCryptMasterKeyGeneration()
-    const demoKeys = new CoverCryptDemoKeys()
-    const hybridEncryption = new CoverCryptHybridEncryption(demoKeys.policy, demoKeys.publicKey)
-    const hybridDecryption = new CoverCryptHybridDecryption(demoKeys.topSecretMkgFinUser)
-    const encryptionDemo = new EncryptionDecryptionDemo(policy,
-      keyGeneration, demoKeys, hybridEncryption, hybridDecryption
-    )
-    encryptionDemo.run()
-    // CoverCryptHybridEncryptionDemo.run()
-  } else {
+//     const keyGeneration = new CoverCryptMasterKeyGeneration()
+//     const demoKeys = new CoverCryptDemoKeys()
+//     const hybridEncryption = new CoverCryptHybridEncryption(demoKeys.policy, demoKeys.publicKey)
+//     const hybridDecryption = new CoverCryptHybridDecryption(demoKeys.topSecretMkgFinUser)
+//     const encryptionDemo = new EncryptionDecryptionDemo(policy,
+//       keyGeneration, demoKeys, hybridEncryption, hybridDecryption
+//     )
+//     encryptionDemo.run()
+//     // CoverCryptHybridEncryptionDemo.run()
+//   } else {
 
+<<<<<<< HEAD
     const policy = new CoverCryptPolicy(100)
       .addAxis("Security Level", ["Protected", "Low Secret", "Medium Secret", "High Secret", "Top Secret"], true)
       .addAxis("Department", ["R&D", "HR", "MKG", "FIN"], false)
@@ -899,6 +1055,24 @@ function abeDemo(): string {
 }
 (window as any).abeDemo = abeDemo
 >>>>>>> 5036a76 (progressing on policy ipl)
+=======
+//     const policy = new CoverCryptPolicy(100)
+//       .addAxis("Security Level", ["Protected", "Low Secret", "Medium Secret", "High Secret", "Top Secret"], true)
+//       .addAxis("Department", ["R&D", "HR", "MKG", "FIN"], false)
+//     const keyGeneration = new GpswMasterKeyGeneration()
+//     const demoKeys = new GpswDemoKeys()
+//     const hybridEncryption = new GpswHybridEncryption(demoKeys.policy, demoKeys.publicKey)
+//     const hybridDecryption = new GpswHybridDecryption(demoKeys.topSecretMkgFinUser)
+//     const encryptionDemo = new EncryptionDecryptionDemo(policy,
+//       keyGeneration, demoKeys, hybridEncryption, hybridDecryption
+//     )
+//     encryptionDemo.run()
+//     // GpswHybridEncryptionDemo.run()
+//   }
+//   return "OK"
+// }
+(window as any).abeDemo = undefined // abeDemo
+>>>>>>> 5706751 (re-importing from old repo)
 
 function elementSetValue(id: string, value: Uint8Array | string) {
   const box = document.getElementById(id)
