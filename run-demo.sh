1c48174cb462134095d28b50bb00d981a4b2df1e
Manuthor <manu.coste@gmail.com>
2022-09-10 07:37:13 +0200

    Add script to populate db

--- run-demo.sh
+++ run-demo.sh
@@ -6,5 +6,6 @@ cd ~/demo_cloudproof/
 docker load <frontend.tar
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up -d
-docker cp dump_db_demo.sql db:/tmp/
-docker exec -i db psql -h localhost -U app_user -d app_db -f /tmp/dump_db_demo.sql
+
+docker cp dump_db_demo.* db:/tmp/
+docker exec -i db sh /tmp/dump_db_demo.sh
