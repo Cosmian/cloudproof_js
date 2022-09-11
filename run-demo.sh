7c947bb7b49fb7502fc81fccc27b9a9482cf29db
Manuthor <manu.coste@gmail.com>
2022-09-11 09:50:03 +0200

    Add delay before provisioning tables

--- run-demo.sh
+++ run-demo.sh
@@ -7,6 +7,7 @@ docker load <frontend.tar
 docker-compose -f docker-compose.yml down
 docker-compose -f docker-compose.yml up -d
 
+sleep 30
 docker cp dump_db_demo.sh db:/tmp/
 docker cp dump_db_demo.sql db:/tmp/
 docker exec -i db sh /tmp/dump_db_demo.sh
