d24905d0ecf07b1dfca8a3d5be0ed11f0f3c9d55
Bruno Grieder <bruno.grieder@cosmian.com>
2022-10-19 11:54:07 +0200

    removed dead code

--- tests/findex/findex.test.ts
+++ tests/findex/findex.test.ts
@@ -166,7 +166,6 @@ test("in memory", async () => {
 })
 
 test("SQLite", async () => {
-  // const sqlite3 = require("sqlite3").verbose()
   const db = new sqlite3.Database(":memory:")
   await new Promise((resolve) => {
     db.run(
