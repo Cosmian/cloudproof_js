ddd60e657d25cd1eb36ff8e0fba017d2ba6d11f4
Thibaud Dauce <ThibaudDauce@users.noreply.github.com>
2022-11-28 10:07:15 +0100

    Try with latest jobs (#69)

--- download_wasm.sh
+++ download_wasm.sh
@@ -1,6 +1,6 @@
-# curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/artifacts/v0.10.0/download?job=build_wasm"
-curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/86101/artifacts"
+# curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/artifacts/v0.10.0/download?job=build_wasm"
+curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/artifacts/develop/download?job=build_wasm"
 unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/findex
 
-curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/86074/artifacts"
+curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/artifacts/v7.1.1/download?job=build_wasm"
 unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/cover_crypt
