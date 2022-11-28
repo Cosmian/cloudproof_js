fc775c9f9a871e60d0a55cdbe78e24d55c32203a
Thibaud Dauce <git@hadibut.fr>
2022-11-28 10:51:37 +0100

    Try with cover_crypt develop

--- download_wasm.sh
+++ download_wasm.sh
@@ -2,5 +2,5 @@
 curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/artifacts/develop/download?job=build_wasm"
 unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/findex
 
-curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/artifacts/v7.1.1/download?job=build_wasm"
+curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/artifacts/develop/download?job=build_wasm"
 unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/cover_crypt
