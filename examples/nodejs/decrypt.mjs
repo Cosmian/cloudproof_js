20da090613e80fde9fd84081033edc86a7835d85
Thibaud Dauce <git@hadibut.fr>
2022-11-29 14:28:22 +0100

    Switch to 2 spaces indentation

--- examples/nodejs/decrypt.mjs
+++ examples/nodejs/decrypt.mjs
@@ -3,40 +3,40 @@ import { CoverCrypt, KmsClient, hexEncode, hexDecode } from "cloudproof_js"
 process.removeAllListeners('warning'); // To remove experimental fetch warnings
 
 (async () => {
-    const useKms = process.argv.includes('--kms');
-
-    const userKeyBytesIndex = process.argv.indexOf('--userKeyBytesHexEncoded') + 1;
-    const userKeyBytes = hexDecode(process.argv[userKeyBytesIndex])
-
-    const userKeyAccessPolicyIndex = process.argv.indexOf('--userKeyAccessPolicy') + 1;
-    const userKeyAccessPolicy = process.argv[userKeyAccessPolicyIndex]
-
-    const userKeyUIDIndex = process.argv.indexOf('--userKeyUID') + 1;
-    let userKeyUID = process.argv[userKeyUIDIndex]
-
-    const encryptedDataHexEncodedIndex = process.argv.indexOf('--encryptedDataHexEncoded') + 1;
-    const encryptedData = hexDecode(process.argv[encryptedDataHexEncodedIndex])
-
-    let result
-    if (useKms) {
-        const client = new KmsClient(new URL("http://localhost:9998/kmip/2_1"))
-
-        if (! userKeyUID) {
-            const uniqueIdentifier = Math.random().toString(36).slice(2, 7);
-            userKeyUID = await client.importCoverCryptUserDecryptionKey(uniqueIdentifier, { bytes: userKeyBytes, policy: userKeyAccessPolicy });
-        }
-
-        result = await client.coverCryptDecrypt(userKeyUID, encryptedData)
-    } else {
-        const { CoverCryptHybridDecryption } = await CoverCrypt();
-
-        const encryption = new CoverCryptHybridDecryption(userKeyBytes);
-
-        result = encryption.decrypt(encryptedData);
+  const useKms = process.argv.includes('--kms');
+  
+  const userKeyBytesIndex = process.argv.indexOf('--userKeyBytesHexEncoded') + 1;
+  const userKeyBytes = hexDecode(process.argv[userKeyBytesIndex])
+  
+  const userKeyAccessPolicyIndex = process.argv.indexOf('--userKeyAccessPolicy') + 1;
+  const userKeyAccessPolicy = process.argv[userKeyAccessPolicyIndex]
+  
+  const userKeyUIDIndex = process.argv.indexOf('--userKeyUID') + 1;
+  let userKeyUID = process.argv[userKeyUIDIndex]
+  
+  const encryptedDataHexEncodedIndex = process.argv.indexOf('--encryptedDataHexEncoded') + 1;
+  const encryptedData = hexDecode(process.argv[encryptedDataHexEncodedIndex])
+  
+  let result
+  if (useKms) {
+    const client = new KmsClient(new URL("http://localhost:9998/kmip/2_1"))
+    
+    if (! userKeyUID) {
+      const uniqueIdentifier = Math.random().toString(36).slice(2, 7);
+      userKeyUID = await client.importCoverCryptUserDecryptionKey(uniqueIdentifier, { bytes: userKeyBytes, policy: userKeyAccessPolicy });
     }
-
-    process.stdout.write(JSON.stringify({
-        headerMetadata: hexEncode(result.headerMetadata),
-        plaintext: hexEncode(result.plaintext),
-    }))
+    
+    result = await client.coverCryptDecrypt(userKeyUID, encryptedData)
+  } else {
+    const { CoverCryptHybridDecryption } = await CoverCrypt();
+    
+    const encryption = new CoverCryptHybridDecryption(userKeyBytes);
+    
+    result = encryption.decrypt(encryptedData);
+  }
+  
+  process.stdout.write(JSON.stringify({
+    headerMetadata: hexEncode(result.headerMetadata),
+    plaintext: hexEncode(result.plaintext),
+  }))
 })()
--- examples/nodejs/encrypt.mjs
+++ examples/nodejs/encrypt.mjs
@@ -4,47 +4,47 @@ import { policy } from './utils.mjs'
 process.removeAllListeners('warning'); // To remove experimental fetch warnings
 
 (async () => {
-    const useKms = process.argv.includes('--kms');
-
-    const publicMasterKeyBytesIndex = process.argv.indexOf('--publicMasterKeyBytesHexEncoded') + 1;
-    const publicMasterKeyBytes = hexDecode(process.argv[publicMasterKeyBytesIndex])
-
-    const publicMasterKeyUIDIndex = process.argv.indexOf('--publicMasterKeyUID') + 1;
-    let publicMasterKeyUID = process.argv[publicMasterKeyUIDIndex]
-
-    const dataToEncryptIndex = process.argv.indexOf('--dataToEncrypt') + 1;
-    const dataToEncrypt = (new TextEncoder).encode(process.argv[dataToEncryptIndex])
-
-    const accessPolicyIndex = process.argv.indexOf('--accessPolicy') + 1;
-    const accessPolicy = process.argv[accessPolicyIndex]
-
-    let metadata
-    if (process.argv.includes('--metadata')) {
-        const metadataIndex = process.argv.indexOf('--metadata') + 1;
-        metadata = (new TextEncoder).encode(process.argv[metadataIndex])
+  const useKms = process.argv.includes('--kms');
+  
+  const publicMasterKeyBytesIndex = process.argv.indexOf('--publicMasterKeyBytesHexEncoded') + 1;
+  const publicMasterKeyBytes = hexDecode(process.argv[publicMasterKeyBytesIndex])
+  
+  const publicMasterKeyUIDIndex = process.argv.indexOf('--publicMasterKeyUID') + 1;
+  let publicMasterKeyUID = process.argv[publicMasterKeyUIDIndex]
+  
+  const dataToEncryptIndex = process.argv.indexOf('--dataToEncrypt') + 1;
+  const dataToEncrypt = (new TextEncoder).encode(process.argv[dataToEncryptIndex])
+  
+  const accessPolicyIndex = process.argv.indexOf('--accessPolicy') + 1;
+  const accessPolicy = process.argv[accessPolicyIndex]
+  
+  let metadata
+  if (process.argv.includes('--metadata')) {
+    const metadataIndex = process.argv.indexOf('--metadata') + 1;
+    metadata = (new TextEncoder).encode(process.argv[metadataIndex])
+  }
+  
+  let encryptedData
+  if (useKms) {
+    const client = new KmsClient(new URL("http://localhost:9998/kmip/2_1"))
+    
+    if (! publicMasterKeyUID) {
+      const uniqueIdentifier = Math.random().toString(36).slice(2, 7);
+      publicMasterKeyUID = await client.importCoverCryptPublicMasterKey(uniqueIdentifier, { bytes: publicMasterKeyBytes, policy });
     }
-
-    let encryptedData
-    if (useKms) {
-        const client = new KmsClient(new URL("http://localhost:9998/kmip/2_1"))
-
-        if (! publicMasterKeyUID) {
-            const uniqueIdentifier = Math.random().toString(36).slice(2, 7);
-            publicMasterKeyUID = await client.importCoverCryptPublicMasterKey(uniqueIdentifier, { bytes: publicMasterKeyBytes, policy });
-        }
-
-        encryptedData = await client.coverCryptEncrypt(publicMasterKeyUID, accessPolicy, dataToEncrypt, {
-            headerMetadata: metadata,
-        })
-    } else {
-        const { CoverCryptHybridEncryption } = await CoverCrypt();
-
-        const encryption = new CoverCryptHybridEncryption(policy, publicMasterKeyBytes);
-
-        encryptedData = encryption.encrypt(accessPolicy, dataToEncrypt, {
-            headerMetadata: metadata,
-        });
-    }
-
-    process.stdout.write(hexEncode(encryptedData));
+    
+    encryptedData = await client.coverCryptEncrypt(publicMasterKeyUID, accessPolicy, dataToEncrypt, {
+      headerMetadata: metadata,
+    })
+  } else {
+    const { CoverCryptHybridEncryption } = await CoverCrypt();
+    
+    const encryption = new CoverCryptHybridEncryption(policy, publicMasterKeyBytes);
+    
+    encryptedData = encryption.encrypt(accessPolicy, dataToEncrypt, {
+      headerMetadata: metadata,
+    });
+  }
+  
+  process.stdout.write(hexEncode(encryptedData));
 })()
