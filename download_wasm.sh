b2db1c800ea663c40af662b16ed22739c29c59d7
Thibaud Dauce <git@hadibut.fr>
2022-11-29 14:42:53 +0100

    Add local downloading/compiling to download_wasm for dev

--- download_wasm.sh
+++ download_wasm.sh
@@ -1,6 +1,40 @@
-# curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/artifacts/v0.10.0/download?job=build_wasm"
-curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/artifacts/develop/download?job=build_wasm"
-unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/findex
+#!/usr/bin/env bash
 
-curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/artifacts/develop/download?job=build_wasm"
-unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/cover_crypt
+FINDEX_VERSION=develop
+COVER_CRYPT_VERSION=develop
+
+rm -rf src/pkg/findex
+rm -rf src/pkg/cover_crypt
+
+SOURCE=${BASH_SOURCE[0]}
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+if [[ -z "${CI_JOB_TOKEN}" ]]; then
+    if [[ -d "../findex2" ]]; then
+        pushd ../findex
+    else
+        git clone git@gitlab.cosmian.com:core/findex.git /tmp/findex
+        pushd /tmp/findex
+    fi
+
+    git checkout $FINDEX_VERSION
+    wasm-pack build --target web -d $DIR/src/pkg/findex --release --features wasm_bindgen
+    popd
+    
+    if [[ -d "../cover_crypt2" ]]; then
+        pushd ../cover_crypt
+    else
+        git clone https://github.com/Cosmian/cover_crypt.git /tmp/cover_crypt
+        pushd /tmp/cover_crypt
+    fi
+
+    git checkout $COVER_CRYPT_VERSION
+    wasm-pack build --target web -d $DIR/src/pkg/cover_crypt --release --features wasm_bindgen
+    popd
+else
+    curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Ffindex/jobs/artifacts/$FINDEX_VERSION/download?job=build_wasm"
+    unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/findex
+
+    curl --location --output artifacts.zip --header "JOB-TOKEN: $CI_JOB_TOKEN" "http://gitlab.cosmian.com/api/v4/projects/core%2Fcover_crypt/jobs/artifacts/$COVER_CRYPT_VERSION/download?job=build_wasm"
+    unzip -o -j artifacts.zip "pkg/bundler/*" -d src/pkg/cover_crypt
+fi
