42cfb4a4697062d670f85c743c88b4d28234574d
Thibaud Dauce <git@hadibut.fr>
2023-02-13 15:13:43 +0100

    Add new wasm functions

--- src/findex/findex_cloud.ts
+++ src/findex/findex_cloud.ts
@@ -1,6 +1,5 @@
 /* eslint-disable jsdoc/require-param */
 /* eslint-disable jsdoc/require-returns */
-import { fromByteArray, toByteArray } from "base64-js"
 import {
   Findex,
   IndexedEntry,
@@ -12,106 +11,9 @@ import {
 import {
   webassembly_search_cloud,
   webassembly_upsert_cloud,
+  webassembly_derive_new_token,
+  webassembly_generate_new_token,
 } from "../pkg/findex/cosmian_findex"
-
-export interface FindexCloudToken {
-  publicId: string
-  findexMasterKey: Uint8Array
-
-  fetchEntriesKey: Uint8Array | null
-  fetchChainsKey: Uint8Array | null
-  upsertEntriesKey: Uint8Array | null
-  insertChainsKey: Uint8Array | null
-}
-
-/**
- * Transform a string to a token object
- */
-function tokenToString(token: FindexCloudToken): string {
-  const masterKeyAndPrivateKey = new Uint8Array([
-    ...token.findexMasterKey,
-    ...(token.fetchEntriesKey !== null ? [0, ...token.fetchEntriesKey] : []),
-    ...(token.fetchChainsKey !== null ? [1, ...token.fetchChainsKey] : []),
-    ...(token.upsertEntriesKey !== null ? [2, ...token.upsertEntriesKey] : []),
-    ...(token.insertChainsKey !== null ? [3, ...token.insertChainsKey] : []),
-  ])
-
-  return token.publicId + fromByteArray(masterKeyAndPrivateKey)
-}
-
-/**
- * Transform a token to a string
- */
-function tokenFromString(tokenAsString: string): FindexCloudToken {
-  const publicId = tokenAsString.slice(0, 5)
-  const bytes = toByteArray(tokenAsString.slice(5))
-  const findexMasterKey = bytes.slice(0, 16)
-  let callbacksKeys = bytes.slice(16)
-
-  const token: FindexCloudToken = {
-    publicId,
-    findexMasterKey,
-
-    fetchEntriesKey: null,
-    fetchChainsKey: null,
-    upsertEntriesKey: null,
-    insertChainsKey: null,
-  }
-
-  while (callbacksKeys.length > 0) {
-    const prefix = callbacksKeys[0]
-    const value = callbacksKeys.slice(1, 16 + 1)
-    callbacksKeys = callbacksKeys.slice(16 + 1)
-
-    if (prefix === 0) {
-      token.fetchEntriesKey = value
-    } else if (prefix === 1) {
-      token.fetchChainsKey = value
-    } else if (prefix === 2) {
-      token.upsertEntriesKey = value
-    } else if (prefix === 3) {
-      token.insertChainsKey = value
-    }
-  }
-
-  return token
-}
-
-/**
- * Create a new token with reduced permissions
- */
-function deriveNewToken(
-  token: FindexCloudToken,
-  permissions: {
-    search: boolean
-    index: boolean
-  } = {
-    search: false,
-    index: false,
-  },
-): FindexCloudToken {
-  const newToken: FindexCloudToken = {
-    publicId: token.publicId,
-    findexMasterKey: token.findexMasterKey,
-    fetchEntriesKey: null,
-    fetchChainsKey: null,
-    upsertEntriesKey: null,
-    insertChainsKey: null,
-  }
-
-  if (permissions.search) {
-    newToken.fetchEntriesKey = token.fetchEntriesKey
-    newToken.fetchChainsKey = token.fetchChainsKey
-  }
-
-  if (permissions.index) {
-    newToken.fetchChainsKey = token.fetchChainsKey
-    newToken.upsertEntriesKey = token.upsertEntriesKey
-    newToken.insertChainsKey = token.insertChainsKey
-  }
-
-  return newToken
-}
 
 /**
  */
@@ -120,9 +22,29 @@ export async function FindexCloud() {
   await Findex()
 
   return {
-    tokenToString,
-    tokenFromString,
-    deriveNewToken,
+    generateNewToken: (
+      indexId: string,
+      fetchEntriesKey: Uint8Array,
+      fetchChainsKey: Uint8Array,
+      upsertEntiesKey: Uint8Array,
+      insertChainsKey: Uint8Array,
+    ): string => {
+      return webassembly_generate_new_token(
+        indexId,
+        fetchEntriesKey,
+        fetchChainsKey,
+        upsertEntiesKey,
+        insertChainsKey,
+      )
+    },
+
+    deriveNewToken: (
+      token: string,
+      search: boolean,
+      index: boolean,
+    ): string => {
+      return webassembly_derive_new_token(token, search, index)
+    },
 
     upsert: async (
       token: string,
